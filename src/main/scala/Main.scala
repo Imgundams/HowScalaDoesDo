3da457cfcf768e07e904dbdb3d23f99100bc32cd
Daniel Raineri <daniel.raineri@qa.com>
2017-07-18 15:16:33 +0100

    Function Done test started

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,26 +1,24 @@
-import scala.util.control.NonFatal
-/**
-	* Created by DRain on 18/07/2017.
-	*/
-object Main extends App {
+import scala.util.{Failure, Success, Try}
 
+// Created by DRain on 18/07/2017.
+object Main extends App {
 	def numberToNumberAndString(aLargeNumber: String): Unit = {
-		val shortScaleList = List("", " thousand ", " million, ", " billion, ", " trillion, ", " quadrillion, ", " quintillion, ", " sextillion, ", " septillion, ", " octillion, ", " nonillion, "," decillion, ")
+		val shortScaleList = List("", " thousand ", " million, ", " billion, ", " trillion, ", " quadrillion, ", " quintillion, ", " sextillion, ", " septillion, ", " octillion, ", " nonillion, ", " decillion, ")
 		val longScaleList = List(" ", " thousand ", " million ", " milliard ", " billion ", " billiard ", " trillion ", " trilliard ", " quadrillion, ", " quadrilliard, ", " quintillion ", " quintilliard, ", " sextillion, ")
 		val stringOfALargeNumber = dotRemover(zeroRemover(aLargeNumber))
 		val doubleOfALargeNumber = stringOfALargeNumber.toDouble
 		stringPrint(stringOfALargeNumber, doubleOfALargeNumber)
 		print("Short scale:")
-		scaleName(stringOfALargeNumber, doubleOfALargeNumber,shortScaleList)
+		scaleName(stringOfALargeNumber, doubleOfALargeNumber, shortScaleList)
 		print("Long scale:")
-		scaleName(stringOfALargeNumber, doubleOfALargeNumber,longScaleList)
+		scaleName(stringOfALargeNumber, doubleOfALargeNumber, longScaleList)
 	}
 
-	def scaleName(stringPart: String, doublePart: Double, format:List[String]): Unit = {
+	def scaleName(stringPart: String, doublePart: Double, format: List[String]): Unit = {
 		val listNumberString = stringPart.reverse.grouped(3).toList.map(_.reverse.toList.mkString)
 		val output = for (i <- listNumberString.indices) yield {
 			i match {
-				case 0 if listNumberString(i) != "000" => "and "+zeroRemover(listNumberString(i))
+				case 0 if listNumberString(i) != "000" => "and " + zeroRemover(listNumberString(i))
 				case _ if listNumberString(i) == "000" => ""
 				case _ if listNumberString(i) != "000" => zeroRemover(listNumberString(i)) + format(i)
 				case _ => ""
@@ -30,20 +28,16 @@ object Main extends App {
 	}
 
 	def dotRemover(stringPart: String): String = {
-		if (stringPart.contains(".")) {
-			stringPart.substring(0, stringPart.indexOf("."))
-		}
-		else {
-			stringPart
+		stringPart match {
+			case _ if stringPart.contains(".") => stringPart.substring(0, stringPart.indexOf("."))
+			case _ => stringPart
 		}
 	}
 
 	def zeroRemover(stringPart: String): String = {
-
-		if (stringPart.charAt(0) == '0') {
-			zeroRemover(stringPart.substring(1))
-		} else {
-			stringPart
+		stringPart match {
+			case _ if stringPart.charAt(0) == '0' => zeroRemover(stringPart.substring(1))
+			case _ => stringPart
 		}
 	}
 
@@ -58,21 +52,18 @@ object Main extends App {
 
 	def InputEntry(): Unit = {
 		println("Enter a Number to parse:\n")
-		try {
+		Try {
 			val scanner = scala.io.StdIn.readLine()
 			println(s"Using the value $scanner")
 			val input = scanner
 			numberToNumberAndString(input)
-		} catch {
-			case NonFatal(exc) => println(s"Error Input! $exc\n Please try again.")
+		} match {
+			case Success(trying) => println("Done!")
+			case Failure(trying) =>
+				println(s"Error Input! $trying\n Please try again.")
 				InputEntry()
-			case _: Throwable => println("Big Error Here. Exiting...")
-				sys.exit(0)
 		}
 	}
 
 	InputEntry()
-
 }
-
-
--- src/test/scala/Tests.scala
+++ src/test/scala/Tests.scala
@@ -1,33 +1,32 @@
 import org.scalatest._
-/**
-	* Created by DRain on 18/07/2017.
-	*/
-class Tests extends FlatSpec{
+
+//Created by DRain on 18/07/2017.
+class Tests extends FlatSpec {
 	//ZERO REMOVER FUNCTION
 	"With a formatted number the Zero return" should "not return any prior zeros" in {
 		val noZeros = "1001"
-		assert(Main.zeroRemover(noZeros)===noZeros)
+		assert(Main.zeroRemover(noZeros) === noZeros)
 	}
-	"With a formatted number with tailing zeros the Zero return" should "not return any prior zeros" in {
+	"With a formatted number, with tailing zeros the Zero return" should "not return any prior zeros" in {
 		val tailingZeros = "100100"
-		assert(Main.zeroRemover(tailingZeros)===tailingZeros)
+		assert(Main.zeroRemover(tailingZeros) === tailingZeros)
 	}
 	"With a non-formatted number the Zero return" should "remove any any prior zeros" in {
 		val someZeros = "001001"
-		assert(Main.zeroRemover(someZeros)==="1001")
+		assert(Main.zeroRemover(someZeros) === "1001")
 	}
 
 	//DOT REMOVER FUNCTION
-	"With a formatted number the with the dot function not affecting the string" should "return itself" in {
+	"With a formatted number and no dots with the dot function" should "return itself" in {
 		val noDotsHere = "1001"
-		assert(Main.dotRemover(noDotsHere)===noDotsHere)
+		assert(Main.dotRemover(noDotsHere) === noDotsHere)
 	}
-	"With a formatted number the with the dot function not affecting the string" should "return itself" in {
-		val noDotsHere = "1001"
-		assert(Main.dotRemover(noDotsHere)===noDotsHere)
+	"With a formatted number the with the dot function to remove the dot " should "return itself without the dot" in {
+		val dotInTheMiddle = "10.01"
+		assert(Main.dotRemover(dotInTheMiddle) === "10")
 	}
-	"With a formatted number the with the dot function not affecting the string" should "return itself" in {
-		val noDotsHere = "1001"
-		assert(Main.dotRemover(noDotsHere)===noDotsHere)
+	"With a formatted number the with the dot function" should "only return numbers before the dot" in {
+		val manyDotsHere = "101.01.010.1..0.0.1"
+		assert(Main.dotRemover(manyDotsHere) === "101")
 	}
 }
